0f2b3dc1edb1236e9362310f383776c9f9293a73
Sneez <ecstanto@ucsc.edu>
2014-10-14 00:00:11 -0700

    updated profile page form queries
    fields in form are handled appropriately swag

--- www/profile/profile.ctrl.js
+++ www/profile/profile.ctrl.js
@@ -27,7 +27,9 @@ angular.module('bander')
           } else {
             console.log('there were no params');
             simpleLogin.getUser().then(function(auth){
+              console.log("outside timeout");
               $timeout(function(){
+                console.log("inside timeout");
                 $scope.auth = auth;
                 var path = 'users/' + auth.uid;
                 $scope.user = fbutil.syncObject(path);
@@ -40,8 +42,16 @@ angular.module('bander')
 
 
 $scope.data = {
- instrument: null 
-}
+ city: '',
+ state: '',
+ guitar: '',
+ bass: '',
+ drums: '',
+ vocals: '',
+ genre: '',
+ influences: '',
+ goals: ''
+};
 
 $scope.submitInfo = function() {
   console.log($scope.data.instrument);
@@ -49,7 +59,15 @@ $scope.submitInfo = function() {
   // $scope.user.$inst().$ref().child('/users/' + $scope.user.uid + '/instrument').set($scope.data.instrument);
 
   console.log($scope.user);
-  $scope.user.$inst().$set("instrument", $scope.data.instrument);
+  $scope.user.$inst().$set("city", $scope.data.city);
+  $scope.user.$inst().$set("state", $scope.data.state);
+  $scope.user.$inst().$set("guitar", $scope.data.guitar);
+  $scope.user.$inst().$set("bass", $scope.data.bass);
+  $scope.user.$inst().$set("drums", $scope.data.drums);
+  $scope.user.$inst().$set("vocals", $scope.data.vocals);
+  $scope.user.$inst().$set("genre", $scope.data.genre);
+  $scope.user.$inst().$set("influences", $scope.data.influences);
+  $scope.user.$inst().$set("goals", $scope.data.goals);
 };
 
 $scope.editMode = function() {
