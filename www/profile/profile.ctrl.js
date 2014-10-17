aa076986a6b0a05aaea195abfc03e5c624d52b5d
Sneez <ecstanto@ucsc.edu>
2014-10-16 20:06:41 -0700

    search tab
    got search with filters working aw hiss

--- www/profile/profile.ctrl.js
+++ www/profile/profile.ctrl.js
@@ -1,14 +1,15 @@
 angular.module('bander')
 
 
-.controller('ProfileCtrl', ['$scope', 'simpleLogin', '$location', '$timeout', '$firebase', 'fbutil', '$stateParams', 'waitForAuth', 
-  function ($scope, simpleLogin, $location, $timeout, $firebase, fbutil, $stateParams, waitForAuth) {
+.controller('ProfileCtrl', function ($scope, simpleLogin, $location, $timeout, $firebase, fbutil, $stateParams, waitForAuth, $ionicNavBarDelegate) {
 
  
         $scope.editing  = false;
         $scope.otherProfile = false;
 
-
+        $scope.goBack = function(){
+          $ionicNavBarDelegate.back();
+        };
 
         $scope.input = {};
 
@@ -40,32 +41,36 @@ angular.module('bander')
 
 
 $scope.data = {
- city: '',
- state: '',
- guitar: '',
- bass: '',
- drums: '',
- vocals: '',
- genre: '',
- influences: '',
- goals: ''
+ // city: '',
+ // state: '',
+ // guitar: '',
+ // bass: '',
+ // drums: '',
+ // vocals: '',
+ // genre: '',
+ // influences: '',
+ // goals: ''
 };
 
 $scope.submitInfo = function() {
-  console.log($scope.data.instrument);
 
   // $scope.user.$inst().$ref().child('/users/' + $scope.user.uid + '/instrument').set($scope.data.instrument);
 
-  console.log($scope.user);
-  $scope.user.$inst().$set("city", $scope.data.city);
-  $scope.user.$inst().$set("state", $scope.data.state);
-  $scope.user.$inst().$set("guitar", $scope.data.guitar);
-  $scope.user.$inst().$set("bass", $scope.data.bass);
-  $scope.user.$inst().$set("drums", $scope.data.drums);
-  $scope.user.$inst().$set("vocals", $scope.data.vocals);
-  $scope.user.$inst().$set("genre", $scope.data.genre);
-  $scope.user.$inst().$set("influences", $scope.data.influences);
-  $scope.user.$inst().$set("goals", $scope.data.goals);
+  console.log($scope.data);
+  // for (prop in $scope.data) {
+  //   if ($scope.data.hasOwnProperty(prop) ) {
+  //     expression
+  //   }
+  // }
+  $scope.user.$inst().$update($scope.data);
+  // $scope.user.$inst().$set("state", $scope.data.state);
+  // $scope.user.$inst().$set("guitar", $scope.data.guitar);
+  // $scope.user.$inst().$set("bass", $scope.data.bass);
+  // $scope.user.$inst().$set("drums", $scope.data.drums);
+  // $scope.user.$inst().$set("vocals", $scope.data.vocals);
+  // $scope.user.$inst().$set("genre", $scope.data.genre);
+  // $scope.user.$inst().$set("influences", $scope.data.influences);
+  // $scope.user.$inst().$set("goals", $scope.data.goals);
 };
 
 $scope.editMode = function() {
@@ -78,4 +83,4 @@ $scope.logout = function() {
   $location.path('/login');
 };
 
-}]);
+});
--- www/search/search.ctrl.js
+++ www/search/search.ctrl.js
@@ -1,4 +1,50 @@
 angular.module('bander')
 
-.controller('SearchCtrl', function($scope) {
+.controller('SearchCtrl', function ($scope, simpleLogin, $location, $timeout, $firebase, fbutil, $stateParams, waitForAuth) {
+	$scope.filters = {};
+
+	$scope.users = fbutil.syncObject('users/');
+
+	$scope.filteredUsers = $scope.users;
+
+	$scope.filter = function(){
+	  if($scope.filters.city || $scope.filters.state || $scope.filters.instrument || $scope.filters.genre){
+		$scope.filteredUsers = $scope.users;
+		console.log($scope.filters);
+		for (user in $scope.users) {
+			if ($scope.users.hasOwnProperty(user) && (user.charAt(0) != "$") ) {
+
+				console.log($scope.filters.city);
+
+				if($scope.filters.city && ($scope.filters.city != $scope.users[user].city) ){
+					$scope.filteredUsers[user] = null;
+					delete $scope.filteredUsers[user];
+				} else if ($scope.filters.state && ($scope.filters.state != $scope.users[user].state) ){
+					$scope.filteredUsers[user] = null;
+					delete $scope.filteredUsers[user];
+				}
+				else if($scope.filters.instrument && ($scope.filters.instrument != 
+					($scope.users[user].guitar || $scope.users[user].bass || $scope.users[user].drums || $scope.users[user].vocals))){
+					$scope.filteredUsers[user] = null;
+					delete $scope.filteredUsers[user];
+				}
+				else if($scope.filters.genre && ($scope.filters.genre != $scope.users[user].genre) ){
+					$scope.filteredUsers[user] = null;
+					delete $scope.filteredUsers[user];
+				}
+			}
+		}
+	  }
+	};
+
+	$scope.clear = function(){
+		$scope.filters.city=null;
+		$scope.filters.state=null;
+		$scope.filters.instrument=null;
+		$scope.filters.genre=null;
+
+		$scope.users = fbutil.syncObject('users/');
+
+		$scope.filteredUsers = $scope.users;
+	};
 });
